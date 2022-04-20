e17ab325ae42ccdc651edb51157339944b72dfd5
yzl <zhdeks@163.com>
2022-04-20 13:22:19 +0800

    合并分支

--- yzl_ssm/src/main/java/yzl/com/pojo/Admin.java
+++ yzl_ssm/src/main/java/yzl/com/pojo/Admin.java
@@ -1,4 +1,14 @@
 package yzl.com.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Admin {
+    private String name;
 }
